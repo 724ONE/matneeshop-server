bb083a0c83882e2e898b61fd616fc33b8a84d806
farihakausar <88570666+farihakausar@users.noreply.github.com>
2025-05-19 12:59:05 +0500

    Update db.js

--- src/database/db.js
+++ src/database/db.js
@@ -6,6 +6,7 @@ const initializeDatabase = () => {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: 3306,
   });
 
   db.connect((err) => {
