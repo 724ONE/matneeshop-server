29d69047d3729f0f396dc5bc8aa6504bb346120e
farihakausar <88570666+farihakausar@users.noreply.github.com>
2025-05-19 11:51:09 +0500

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get("/", (req, res) => {
 
 loadAppRoutes(app);
 
-const PORT = process.env.PORT || 3001;
+const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
