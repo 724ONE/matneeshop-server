17003b68684cbed6432bb700e105e1d441aec6f0
farihakausar <fariha@724.one>
2025-06-03 13:14:04 +0500

    Update db.js

--- src/database/db.js
+++ src/database/db.js
@@ -3,14 +3,8 @@ const mysql = require("mysql2");
 const isProduction = process.env.NODE_ENV === "production";
 const initializeDatabase = () => {
   const db = mysql.createConnection({
-    ...(isProduction
-      ? {
-          socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
-        }
-      : {
-          host: process.env.DB_HOST,
-          port: 3306,
-        }),
+    host: process.env.DB_HOST,
+    port: 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
